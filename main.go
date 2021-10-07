22d08ce83227d569bee9be28ab5b6ff00c46cc63
Løvgren, Dag Erik <dagerikhl@gmail.com>
2021-10-07 19:49:35 +0200

    Change starter API to characters instead of albums

--- main.go
+++ main.go
@@ -6,61 +6,63 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+// character represents data about a character.
+type character struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	InPlay       bool   `json:"inPlay"`
+	Strength     int    `json:"strength"`
+	Intelligence int    `json:"intelligence"`
+	Charisma     int    `json:"charisma"`
 }
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+// characters slice to seed character data.
+var characters = []character{
+	{ID: "1", Name: "Kelsier", InPlay: true, Strength: 16, Intelligence: 9, Charisma: 16},
+	{ID: "2", Name: "Shallan Devar", InPlay: true, Strength: 15, Intelligence: 17, Charisma: 9},
+	{ID: "3", Name: "Jarl Berserkarson", InPlay: false, Strength: 18, Intelligence: 3, Charisma: 18},
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
-	router.GET("/albums/:id", getAlbumByID)
+	router.GET("/characters", getCharacters)
+	router.POST("/characters", postCharacters)
+	router.GET("/characters/:id", getCharacterByID)
 
 	router.Run("localhost:8080")
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+// getCharacters responds with the list of all characters as JSON.
+func getCharacters(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, characters)
 }
 
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+// postCharacters adds an character from JSON received in the request body.
+func postCharacters(c *gin.Context) {
+	var newCharacter character
 
-	// Call BindJSON to bind the received JSON to newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	// Call BindJSON to bind the received JSON to newCharacter.
+	if err := c.BindJSON(&newCharacter); err != nil {
 		return
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	// Add the new character to the slice.
+	characters = append(characters, newCharacter)
+	c.IndentedJSON(http.StatusCreated, newCharacter)
 }
 
-// getAlbumByID locates the album whose ID value matches the id parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
+// getCharacterByID locates the character whose ID value matches the id parameter sent by the client, then returns that character as a response.
+func getCharacterByID(c *gin.Context) {
 	id := c.Param("id")
 
-	// Loop over the list of albums, looking for an album whose ID value matches the parameter.
-	for _, album := range albums {
-		if album.ID == id {
-			c.IndentedJSON(http.StatusOK, album)
+	// Loop over the list of characters, looking for an character whose ID value matches the parameter.
+	for _, character := range characters {
+		if character.ID == id {
+			c.IndentedJSON(http.StatusOK, character)
 
 			return
 		}
 	}
 
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found."})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Character not found."})
 }
